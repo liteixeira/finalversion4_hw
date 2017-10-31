707df66b36447f97e237652c9257b658bcf32d1f
Sumeet Dhakan <ldemesqu@asu.edu>
2017-10-31 14:53:58 -0700

    Add alert message for the login view controller and create a class called login data

--- HW4/LoginVC.swift
+++ HW4/LoginVC.swift
@@ -8,8 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginVC : UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var textuserName: UITextField!
+    
+    @IBOutlet weak var textPassword: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +23,11 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+  // Hide keyboard when user touches outside the keyboard
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
 
 }
 
---
+++ HW4/logindata.swift
@@ -0,0 +1,23 @@
+//
+//  logindata.swift
+//  HW4
+//
+//  Created by Sumeet Dhakan on 10/30/17.
+//  Copyright © 2017 livia. All rights reserved.
+//
+
+import Foundation
+
+class LoginData {
+    // TODO: Define my LoginData class
+    // Needs: properties for email, password and class initializer
+    
+    let email: String
+    let password: String
+    
+    init(email: String, password:String) {
+        self.email = email
+        self.password = password
+    }
+    
+}
