0b73cdd5ed333a45ffe2c11597fd0f93e8bac76b
Sumeet Dhakan <ldemesqu@asu.edu>
2017-10-31 23:13:13 -0700

    Add registration buttons in registration VC Add msgs alerts create login data class

--- HW4/RegistrationVC.swift
+++ HW4/RegistrationVC.swift
@@ -12,15 +12,55 @@ import UIKit
 var userEmail:String = ""
 var userPassword:String = ""
 var userRepeatPassword:String = ""
-
+var track:String = ""
+var user_Login = [LoginData]()
 
 class RegistrationVC: UIViewController {
     
+    @IBOutlet weak var helperButton: UIButton!
     @IBOutlet weak var emailTextField: UITextField!
-    
+    @IBOutlet weak var beneficiaryButton: UIButton!
     @IBOutlet weak var passwordTextField: UITextField!
-    
+    @IBOutlet weak var organizationButton: UIButton!
     @IBOutlet weak var repeatpasswordTextField: UITextField!
+    
+    
+    @IBAction func beneficiaryPressed(_ sender: UIButton) {
+        
+        beneficiaryButton.backgroundColor = UIColor.init(displayP3Red: 0.0, green: 0.5, blue: 0.5, alpha: 1.0)
+        beneficiaryButton.setTitleColor(UIColor.white, for: .normal)
+        helperButton.backgroundColor = UIColor.init(displayP3Red: 0.0, green: 0.5, blue: 0.5, alpha: 0.5)
+        helperButton.setTitleColor(UIColor.black, for: .normal)
+        organizationButton.backgroundColor = UIColor.init(displayP3Red: 0.0, green: 0.5, blue: 0.5, alpha: 0.5)
+        organizationButton.setTitleColor(UIColor.black, for: .normal)
+        track = "Beneficiary"
+        print("\(track)")
+    }
+    
+    @IBAction func helperPressed(_ sender: UIButton) {
+        beneficiaryButton.backgroundColor = UIColor.init(displayP3Red: 0.0, green: 0.5, blue: 0.5, alpha: 0.5)
+        beneficiaryButton.setTitleColor(UIColor.black, for: .normal)
+        helperButton.backgroundColor = UIColor.init(displayP3Red: 0.0, green: 0.5, blue: 0.5, alpha: 1.0)
+        helperButton.setTitleColor(UIColor.white, for: .normal)
+        organizationButton.backgroundColor = UIColor.init(displayP3Red: 0.0, green: 0.5, blue: 0.5, alpha: 0.5)
+        organizationButton.setTitleColor(UIColor.black, for: .normal)
+        track = "Helper"
+        print("\(track)")
+
+    }
+    
+    @IBAction func organizationPressed(_ sender: UIButton) {
+        beneficiaryButton.backgroundColor = UIColor.init(displayP3Red: 0.0, green: 0.5, blue: 0.5, alpha: 0.5)
+        beneficiaryButton.setTitleColor(UIColor.black, for: .normal)
+        helperButton.backgroundColor = UIColor.init(displayP3Red: 0.0, green: 0.5, blue: 0.5, alpha: 0.5)
+        helperButton.setTitleColor(UIColor.black, for: .normal)
+        organizationButton.backgroundColor = UIColor.init(displayP3Red: 0.0, green: 0.5, blue: 0.5, alpha: 1.0)
+        organizationButton.setTitleColor(UIColor.white, for: .normal)
+        track = "Organization"
+        print("\(track)")
+
+    }
+    
     
     // Hide keyboard when user touches outside the keyboard
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -33,29 +73,47 @@ class RegistrationVC: UIViewController {
     // Testing if register button was pressed
         print("Pressed")
         
+        
       ////////////////////////// Extractes text fields /////////////////
         userEmail = emailTextField.text!
         userPassword = passwordTextField.text!
         userRepeatPassword = repeatpasswordTextField.text!
       ///////////////////////////////////////////////////////////////////
-        
+      
+     
         
       ////////////////////////// Check for empty fields and create an alert msg for user //////////////////////////
-       if (userEmail.isEmpty || userPassword.isEmpty || userRepeatPassword.isEmpty){
+       if (userEmail.isEmpty || userPassword.isEmpty || userRepeatPassword.isEmpty) && track.isEmpty {
         // Alert message that all fields are required
         print("empty")
-        msgAlert(title: "Alert!", text: "All fields are required")
+        msgAlert(title: "Alert!", text: "All fields are required. Including registering either as Beneficiary, Helper or Organization.")
         }
+        if (userEmail.isEmpty || userPassword.isEmpty || userRepeatPassword.isEmpty) && track.isEmpty == false {
+            // Alert message that all fields are required
+            print("empty")
+            msgAlert(title: "Alert!", text: "Email, password and repeat password fields are required.")
+        }
+        
+        
        ///////////////////////////////////////////////////////////////////////////////////////////////////////////
         
         
         
       ////// Check if password is == repeat password and alert text confirming successful registration //////////
-        if userPassword != userRepeatPassword {
-            msgAlert(title: "Warning!", text: "Password and repeat password are different. Type again")
+        if (userPassword != userRepeatPassword) {
+            msgAlert(title: "Warning!", text: "Password and repeat password are different. Type again.")
+        } else if (track.isEmpty) && (userEmail.isEmpty == false || userPassword.isEmpty == false || userRepeatPassword.isEmpty == false) {
+            msgAlert(title: "Warning", text: "You still need to register as either Beneficiary, Helper or Organization.")
         } else {
-            msgConfirmation(title: "Confirmation", text: "You successfully registered")
+          msgConfirmation(title: "Confirmation", text: "You successfully registered")
+            user_Login.append(LoginData(email: userEmail, password: userPassword, usertype: track ))
+            for item in user_Login {
+            print("\(item.email)")
+            print("\(item.password)")
+            print("\(item.usertype)")
+            }
         }
+        
        //////////////////////////////////////////////////////////////////////////////////////////////////////////
       
         // Store data
@@ -67,10 +125,16 @@ class RegistrationVC: UIViewController {
         
     } // end of registerButton
     
+    
+    
+    
+    
+    
+    
     /////////////////////////////////// Function msgConfirmation generates confirmation text /////////////////////////////
     
     func msgConfirmation (title:String, text:String) {
-        print("Being used")
+
         let confirmation = UIAlertController.init(title: title, message: text, preferredStyle: .alert)
         let confirmationAction = UIAlertAction.init(title: "OK", style: .default) { (action) in
             confirmation.dismiss(animated: true, completion: nil)
