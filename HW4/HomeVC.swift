a5fd07e49eeefd1ee18202dd4e05f9e0c3f2979f
Sumeet Dhakan <ldemesqu@asu.edu>
2017-11-02 23:17:38 -0700

    programmed the home page buttons

--- HW4/HomeVC.swift
+++ HW4/HomeVC.swift
@@ -17,16 +17,47 @@ class HomeVC: UIViewController {
     var current_User_Name:String = ""
     var current_User_Email:String = ""
     
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var button4: UIButton!
     @IBOutlet weak var welcometext: UILabel!
-    
+
+
     @IBAction func logoutpressed(_ sender: UIBarButtonItem) {
         self.dismiss(animated: false, completion: nil)
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         welcometext.text = "Welcome \(current_User_Name) "
+        
+        switch current_User_Account {
+        case "Beneficiary":
+            button1.setTitle("Warning Center", for: .normal)
+            button2.setTitle("My Profile", for: .normal)
+            button3.setTitle("Service Monitoring", for: .normal)
+            button4.setTitle("Organizations", for: .normal)
+        case "Helper":
+            
+            button1.setTitle("Warning Center", for: .normal)
+            button2.setTitle("My Profile", for: .normal)
+            button3.setTitle("Service Monitoring", for: .normal)
+            button4.removeFromSuperview()
+            
+        case "Organization":
+            button1.setTitle("Warning Center", for: .normal)
+            button2.setTitle("My Profile", for: .normal)
+            button3.setTitle("Service Monitoring", for: .normal)
+            button4.removeFromSuperview()
+            
+        default:
+            print("Error! Check the code!!")
+        }
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
